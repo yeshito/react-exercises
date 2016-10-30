768c95e1f2e825e5c3195a55db65ec03c4d80f0c
Yeshe Wingerd <yeshito@gmail.com>
2016-10-29 21:45:38 -0700

    added timerform

--- time_tracking_app/public/app.js
+++ time_tracking_app/public/app.js
@@ -22,7 +22,7 @@ const EditableTimerList = React.createClass({
           runningSince={null}
           editFormOpen={false}
         />
-        <EditableTimerList
+        <EditableTimer
           title='Learn extreme ironing'
           project='World Domination'
           elapsed='3890985'
@@ -31,5 +31,51 @@ const EditableTimerList = React.createClass({
           />
       </div>
     )
-  }
-})
+  },
+});
+const EditableTimer = React.createClass({
+  render: function () {
+    if (this.props.editFormOpen) {
+      return (
+        <TimerForm
+          title={this.props.title}
+          project={this.props.project}
+        />
+      );
+    } else {
+      return (
+        <Timer
+          title={this.props.title}
+          project={this.props.project}
+          elapsed={this.props.elapsed}
+          runningSince={this.props.runningSince}
+        />
+      )
+    };
+  },
+});
+const TimerForm = React.createClass({
+  render: function () {
+    const submitText = this.props.title ? 'Update' : 'Create';
+      return (
+        <div className='ui centered card'>
+          <div className='content'>
+            <div className='ui form'>
+              <div className='field'>
+                <label>Title</label>
+                <input type='text' defaultValue={this.props.value} />
+                <div className='field'>
+                  <label>Project</label>
+                  <input type='text' defaultValue={this.props.project} />
+                </div>
+                <div className='ui two bottom attached buttons'>
+                  <button className='ui basic blue button'>{submitText}</button>
+                  <button className='ui basic red button'>Cancel</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+  },
+});
