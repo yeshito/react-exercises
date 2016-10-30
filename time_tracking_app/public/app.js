3ae59160e435586145fdf8849ef957e5351be559
Yeshe Wingerd <yeshito@gmail.com>
2016-10-29 21:33:35 -0700

    added two components

--- time_tracking_app/public/app.js
+++ time_tracking_app/public/app.js
@@ -1,1 +1,35 @@
 
+const TimersDashboard = React.createClass({
+  render: function () {
+    return (
+      <div className='ui three column centered grid'>
+        <div className='column'>
+          <EditableTimerList />
+          <ToggleableTimerForm isOpen={true} />
+        </div>
+      </div>
+    );
+  },
+});
+const EditableTimerList = React.createClass({
+  render: function () {
+    return (
+      <div id='timers'>
+        <EditableTimer
+          title='Learn React'
+          project='Web Domination'
+          elapsed='8986300'
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimerList
+          title='Learn extreme ironing'
+          project='World Domination'
+          elapsed='3890985'
+          runningSince={null}
+          editFormOpen={true}
+          />
+      </div>
+    )
+  }
+})
